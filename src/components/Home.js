d6e3029f8d8dc2f03c2a146f9239f4df5d25bc28
piotr <piotr.jankowski2204@gmail.com>
2021-12-17 12:44:02 +0000

    portfolio react app working with semantic ui

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header, Segment } from "semantic-ui-react";
+
+const Home = () => {
+  return(
+    <Segment textAlign='center'>
+      <Header as='h1'>Home page</Header>
+    </Segment>
+  )
+}
+
+export default Home
