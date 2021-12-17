5f0c452b94081e929d260f03e27e60eda021ec79
piotr <piotr.jankowski2204@gmail.com>
2021-12-17 13:18:21 +0000

    portfolio react app working with semantic ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Header, Segment } from "semantic-ui-react";
 
 const Home = () => {
-  return(
-    <Segment textAlign='center'>
-      <Header as='h1'>Home page</Header>
+  return (
+    <Segment textAlign="center">
+      <Header as="h1">Home page</Header>
     </Segment>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
