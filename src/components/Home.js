d6baad7a008cf814ae48286f68722aaa2cc85c8f
piotr <piotr.jankowski2204@gmail.com>
2021-12-29 22:23:48 +0000

    text animations added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,49 @@
 import React from "react";
-import { Container, Header, Segment } from "semantic-ui-react";
+import { Container, Segment } from "semantic-ui-react";
+import Typical from "react-typical";
 
 const Home = () => {
   return (
     <Container>
-    <Segment textAlign="center" raised inverted>
-      <Header as="h1">Hello there!</Header>
-      <Segment raised>
-        <Header as='h2'>Thank you for visiting my site!</Header>
-        <Header as='h4'>As you can see, I am still in progress!<br/>Catch you later!<br/>Regards, Piotr!</Header>
+      <Segment textAlign="center" raised inverted>
+        <Typical
+        steps={[500,'Hello there!', 1000, ""]}
+        loop={Infinity}
+        wrapper="h1"
+      />
+        <Segment raised>
+          <Typical
+            steps={[1000,
+              "Thank you for visiting my site!",
+            ]}
+            loop={1}
+            wrapper="h2"
+          />
+          <Typical
+            steps={[1000,
+              " As you can see, I am still in progress!",
+            ]}
+            loop={1}
+            wrapper="h4"
+          />
+          <Typical
+            steps={[1000,
+              "Catch you later!",
+            ]}
+            loop={1}
+            wrapper="h4"
+          />
+          <Typical
+            steps={[1000,
+              "Regards, Piotr!",
+            ]}
+            loop={1}
+            wrapper="h4"
+          />
+        </Segment>
       </Segment>
-    </Segment>
     </Container>
   );
 };
 
-export default Home
+export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,27 +8,32 @@ import { motion } from "framer-motion/dist/framer-motion";
 const Navbar = () => {
   return (
     <Menu size="tiny">
-      <Menu.Item>
-        <Header as={("h4", Link)} to="/" className="myname">
-          <Image src={me} size="tiny"></Image>
-          Piotr Jankowski
-        </Header>
-      </Menu.Item>
-      <Menu.Menu className='buttonsMotion' position="right">
-        <motion.div whileHover={{ scale: 1.6}}>
+      <motion.circle animate={{ x: [0, 200, 0] }} transition={{ ease: "easeIn" }}>
+        <Menu.Item>
+          <Header as={("h4", Link)} to="/" className="myname">
+            <Image src={me} size="tiny"></Image>
+            Piotr Jankowski
+          </Header>
+        </Menu.Item>
+      </motion.circle>
+      <Menu.Menu className="buttonsMotion" position="right">
+      <motion.circle className='motion' animate={{ x: [0, -200, 0] }} transition={{ ease: "easeIn" }}>
+        <motion.div whileHover={{ scale: 1.6 }}>
           <Menu.Item as={Link} to="/expiriance">
             Expiriance
           </Menu.Item>
         </motion.div>
-        <motion.div whileHover={{ scale: 1.6}}>
-        <Menu.Item as={Link} to="/portfolio">
-          Portfolio
-        </Menu.Item>
+        <motion.div whileHover={{ scale: 1.6 }}>
+          <Menu.Item as={Link} to="/portfolio">
+            Portfolio
+          </Menu.Item>
         </motion.div>
-        <motion.div whileHover={{ scale: 1.6}}>
-        <ContactMe />
+        <motion.div whileHover={{ scale: 1.6 }}>
+          <ContactMe />
         </motion.div>
+        </motion.circle>
       </Menu.Menu>
+      
     </Menu>
   );
 };
